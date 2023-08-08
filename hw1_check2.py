871c7147f5a5d4da6d40f7d4ce54ff03b9ae87b7
Yvonne Galusha <64611377+yvonnegalusha@users.noreply.github.com>
2023-08-08 17:40:09 -0500

    Add files via upload

---
+++ hw1_check2.py
@@ -0,0 +1,40 @@
+"""
+Tests BAIS:6060 Homework 1
+"""
+from io import StringIO 
+import filecmp
+import os.path
+import sys
+
+class Capturing(list):
+    def __enter__(self):
+        self._stdout = sys.stdout
+        sys.stdout = self._stringio = StringIO()
+        return self
+    def __exit__(self, *args):
+        self.extend(self._stringio.getvalue().splitlines())
+        del self._stringio    # free up some memory
+        sys.stdout = self._stdout
+        
+
+def file1():
+
+    try:
+        exists = os.path.exists("lasers_small.csv")
+        assert exists == True
+    except:
+        sys.exit()
+    
+    with Capturing() as output:  
+        f1 = "output.txt"
+        f2 = "a.txt"
+        result = filecmp.cmp(f1, f2)
+        print(result)
+
+    assert output == ['True']
+
+def main():
+    file1()
+
+if __name__ == "__main__":
+    main()
